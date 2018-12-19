be547cecb927d86f93dd82890479f72b2814d144
Mark Perity <mperity@gmail.com>
2018-12-19 17:56:32 +0000

    another try to connect to mlab correctly

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose');
 
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://markpeartree:Pa55w0rd@ds139614.mlab.com:39614/todoapp'
+}
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://markpeartree:Pa55w0rd@ds139614.mlab.com:39614/todoapp', { useNewUrlParser: true });
+mongoose.connect(db.mlab || db.localhost);
 
 module.exports = {
   mongoose
