c9e50d0ced016dbace634085ad7cfc831f3bb859
Mark Perity <mperity@gmail.com>
2018-12-19 17:47:10 +0000

    updated link to mLab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://markpeartree:P@55w0rd@ds139614.mlab.com:39614/todoapp', { useNewUrlParser: true });
+mongoose.connect('mongodb://markpeartree:Pa55w0rd@ds139614.mlab.com:39614/todoapp', { useNewUrlParser: true });
 
 module.exports = {
   mongoose
