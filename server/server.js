131f0e64d96221ee4b6451171cd6782592127b8d
Mark Perity <mperity@gmail.com>
2018-12-27 18:59:59 +0000

    This is a trial to commit

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {user} = require('./models/user');
+var {User} = require('./models/user');
 
 
 var app = express();
@@ -67,6 +67,19 @@ app.delete('/todos/:id', (req,res) => {
     res.status(404).send();
   });
 }
+})
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
 })
 
 app.listen(port, () => {
